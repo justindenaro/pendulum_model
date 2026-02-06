db53f7cd3f8d11a307557d27c74872a4eb11147c
justindenaro <justindenaro98@gmail.com>
2026-02-05 16:07:55 -0800

    fixed t_eval

--- 1dof_pend.py
+++ 1dof_pend.py
@@ -18,7 +18,7 @@ def pendcart(t, y, M, m, l, I, g, F, b):
 
     # Modified accelerations including damping b
     x_accel = ((I + m*l**2)*(F_eff + m*l*theta_dot**2*st) - (m**2*l**2*g*ct*st)) / det
-    theta_accel = ((M+m)*m*g*l*st - m*l*ct*(F_eff + m*l*theta_dot**2*st)) / det
+    theta_accel = ((M + m)*m*g*l*st - m*l*ct*(F_eff + m*l*theta_dot**2*st)) / det
 
     return [x_dot, x_accel, theta_dot, theta_accel]
 
@@ -43,10 +43,10 @@ params = (M, m, l, I, g, F, b)
 x0 = 0.0 # initial cart position (m)
 v0 = 0.1 #initial cart velocity (m/s)
 theta0 = np.pi/2 # initial pendulum angle (radians)
-omega0 = 9.9 # initial angular velocity (rad/s)
+omega0 = 0.0 # initial angular velocity (rad/s)
 y0 = [x0, v0, theta0, omega0]   # Initial state vector
 t_span = (0, 60) #time span for simulation
-t_eval = (0, 60, 1000) # time points, 1000 from 0-60 seconds
+t_eval = np.linspace(0, 60, 1000) # time points, 1000 from 0-60 seconds
 
 # Solve ODE
 sol = solve_ivp(
